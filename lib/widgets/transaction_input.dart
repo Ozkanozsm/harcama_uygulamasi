bddb409f3e39e8c74e3bbce61f6fd34ad0f2173a
Ozkan Ozesme <ozkanozsm@gmail.com>
2021-07-08 19:18:25 +0300

    Change keyboardType of amount TextField to number

--- lib/widgets/transaction_input.dart
+++ lib/widgets/transaction_input.dart
@@ -23,6 +23,7 @@ class TransactionInput extends StatelessWidget {
             TextField(
               decoration: InputDecoration(labelText: "Amount"),
               controller: amountController,
+              keyboardType: TextInputType.number,
               // onChanged: (value) => amountInput = value,
             ),
             TextButton(
