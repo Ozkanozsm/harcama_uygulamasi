7690d4ffe1215b7eecbd048e48b740b410a5941e
Ozkan Ozesme <ozkanozsm@gmail.com>
2021-07-08 19:14:41 +0300

    Change SingleChildScrollView to ListView.builder

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -12,73 +12,72 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 250,
-      child: SingleChildScrollView(
-        child: Column(
-          children: transactions
-              .map((tx) => Card(
-                    color: Colors.teal,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.vertical(
-                        bottom: Radius.circular(15),
-                      ),
+      child: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Card(
+            color: Colors.teal,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.vertical(
+                bottom: Radius.circular(15),
+              ),
+            ),
+            elevation: 25,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.green),
                     ),
-                    elevation: 25,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Container(
-                          child: ElevatedButton(
-                            style: ButtonStyle(
-                              backgroundColor: MaterialStateProperty.all<Color>(
-                                  Colors.green),
-                            ),
-                            child: Icon(
-                              Icons.attach_money_outlined,
-                              color: Colors.white,
-                            ),
-                            onPressed: null,
-                          ),
-                          margin:
-                              EdgeInsets.symmetric(vertical: 5, horizontal: 20),
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              tx.title,
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                            Text(
-                              DateFormat('E, dd-MM-yy').format(tx.date),
-                              style: TextStyle(
-                                  color: Colors.red,
-                                  backgroundColor: Colors.cyanAccent,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                          ],
-                        ),
-                        Container(
-                          margin: EdgeInsets.all(10),
-                          padding: EdgeInsets.all(10),
-                          decoration: BoxDecoration(
-                            border: Border.all(
-                              color: Colors.amber,
-                              width: 5,
-                            ),
-                          ),
-                          child: Text(
-                            // TODO fix
-                            tx.amount.toString().padLeft(5, "0"),
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                      ],
+                    child: Icon(
+                      Icons.attach_money_outlined,
+                      color: Colors.white,
                     ),
-                  ))
-              .toList(),
-        ),
+                    onPressed: null,
+                  ),
+                  margin: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      transactions[index].title,
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      DateFormat('E, dd-MM-yy')
+                          .format(transactions[index].date),
+                      style: TextStyle(
+                          color: Colors.red,
+                          backgroundColor: Colors.cyanAccent,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.amber,
+                      width: 5,
+                    ),
+                  ),
+                  child: Text(
+                    // TODO fix
+                    transactions[index].amount.toString().padLeft(5, "0"),
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+        itemCount: transactions.length,
       ),
     );
   }
